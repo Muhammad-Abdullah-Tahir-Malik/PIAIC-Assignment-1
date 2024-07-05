5cbaff00b34b938dc7cdc3115bf8094a92c353c2
unknown <abdullah.tahir.001177@gmail.com>
2024-07-05 11:55:30 +0500

    commit

---
+++ question9.ts
@@ -0,0 +1,8 @@
+/*Favorite Number: Store your favorite number in a variable. Then, using 
+that variable, create a message that reveals your favorite number. Print 
+that message.
+
+ */
+
+let num:number=7;
+console.log(`Your favourite number is ${num}`);
