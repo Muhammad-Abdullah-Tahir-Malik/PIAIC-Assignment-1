ec906c59d80653ab719cd983ca0fa0a010f58839
unknown <abdullah.tahir.001177@gmail.com>
2024-07-05 12:12:57 +0500

    commit

---
+++ question12.ts
@@ -0,0 +1,13 @@
+/*Greetings: Start with the array you used in Exercise 11, but instead of
+ just printing each person’s name, print a message to them. The text of
+  each message should be the same, but each message should be personalized
+   with the person’s name.
+ */
+
+
+   let arr:string[]=["Zaid","Mati","Mahad"];
+for(let i=0;i<arr.length;i++){
+    console.log("Hi, how are you "+arr[i]);
+}
+
+export{}
