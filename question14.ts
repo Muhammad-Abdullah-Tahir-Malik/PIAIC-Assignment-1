85b92557102a68c4ab80c38207d5bee2d745e21a
unknown <abdullah.tahir.001177@gmail.com>
2024-07-10 02:04:53 +0500

    commit

---
+++ question14.ts
@@ -0,0 +1,14 @@
+/*Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make
+a list that includes at least three people you’d like to invite to dinner. Then use your list to print
+a message to each person, inviting them to dinner. */
+
+
+let arr:string[]=["Grandmothes","Mother","Prophet (PBUH)"];
+
+for(let i=0;i<arr.length;i++){
+    console.log(`A.o.A, dear ${arr[i]} I would like to invite you all to the dinner`);
+    
+}
+
+
+export{}
