02e751aa536a59e943ecf86009639839acc0cc46
unknown <abdullah.tahir.001177@gmail.com>
2024-07-10 16:05:28 +0500

    commit

---
+++ question22.ts
@@ -0,0 +1,10 @@
+/*Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program. */
+
+const items = ['apple', 'banana', 'cherry'];
+
+console.log('The first item is:', items[0]);
+console.log('The second item is:', items[1]);
+console.log('The third item is:', items[2]);
+//console.log('The fourth item is:', items[3]); 
+
+export{}
