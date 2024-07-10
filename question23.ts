9ee53fd9778bd4d7156534e7f474c1addfedd24a
unknown <abdullah.tahir.001177@gmail.com>
2024-07-10 16:46:26 +0500

    commit

---
+++ question23.ts
@@ -0,0 +1,33 @@
+/* Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:*/
+
+let car="honda"
+console.log(car=="honda");
+
+let num=7;
+console.log(num==7);
+
+let float=0.565;
+console.log(float==0.565);
+
+let bike="YBR";
+console.log(bike=="YBR");
+
+let player="Jimmy";
+console.log(player=="Jimmy");
+
+let num1=3;
+var b=(num1==2)? "True":"False";
+console.log(b);
+
+let nam="Raza";
+var nam1=(nam=="Ali")? "True":"False";
+console.log(nam1);
+
+
+let fav="Ronalda";
+var fav1=(fav=="Messi")? "True":"False";
+console.log(fav1);
+
+
+
+export{}
