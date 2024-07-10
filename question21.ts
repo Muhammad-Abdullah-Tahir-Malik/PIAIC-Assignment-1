2ba74277a8e0a990cf6bfc985e0698eae3a93345
unknown <abdullah.tahir.001177@gmail.com>
2024-07-10 16:00:13 +0500

    commit

---
+++ question21.ts
@@ -0,0 +1,12 @@
+/*They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items. */
+
+let obj={
+    name:"Ali",
+    Course:"BSCS",
+    Enrollment: 7,
+
+}
+
+console.log(obj.name+"\t"+obj.Enrollment+"\t"+obj.Course);
+
+export{}
