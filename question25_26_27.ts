7ae1bbf610d4e714f87bbff74ccc4bd4be7e7bdb
unknown <abdullah.tahir.001177@gmail.com>
2024-07-10 17:01:09 +0500

    commit

---
+++ question25_26_27.ts
@@ -0,0 +1,24 @@
+
+
+
+let alien_game="green";
+
+if(alien_game=="green"){
+    console.log("You earn 5 points");
+    
+}
+
+else if(alien_game=="yellow"){
+    console.log("You earn 10 points");
+    
+}
+
+else if(alien_game=="red"){
+    console.log("You earn 15 points");
+    
+}
+
+else {
+    console.log("You don't earn any points");
+    
+}
