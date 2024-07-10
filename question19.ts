7a19f543d62b847b537698ca606858ef4a7097d4
unknown <abdullah.tahir.001177@gmail.com>
2024-07-10 14:25:07 +0500

    commit

---
+++ question19.ts
@@ -0,0 +1,12 @@
+/*Dinner Guests: Working with one of the programs from Exercises 14 through
+18, print a message indicating the number of people you are inviting to
+dinner. */
+
+let arr:string[]=["Ali","Ahmad","Raza","Rafiq"];
+
+for(let i=0;i<arr.length;i++){
+    console.log(i);
+    
+}
+
+export{}
