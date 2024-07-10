23748803dd4234fce61a1857a8aeb4365b5b2e7e
unknown <abdullah.tahir.001177@gmail.com>
2024-07-10 14:21:04 +0500

    commit

---
+++ question18.ts
@@ -0,0 +1,23 @@
+/*Seeing the World: Think of at least five places in the world you’d like
+to visit.*/
+
+let arr:string[]=["Murry","Naltar","Gilgit","Naran","Nathia"];
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+    
+}
+
+
+arr.sort();
+for(let i=0;i<arr.length;i++){
+    console.log(`${arr[i]}`);
+    
+}
+
+arr.reverse();
+for(let i=0;i<arr.length;i++){
+    console.log(`${arr[i]}`);
+    
+}
+
+export{}
